25bd45d6c9e3c95db92a23edf0d43ae5b052fa6b
Vasilij Yanikeev <yanvasilij@gmail.com>
2018-09-16 11:26:03 +0500

    Added reg_dbg_data scripts

---
+++ cli_test/reg_dbg_data.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Beremiz_XCP43\wx30-venv\Scripts\python.exe" reg_dbg_data.py
+pause
---
+++ cli_test/reg_dbg_data.py
@@ -0,0 +1,25 @@
+import serial
+import time
+from cli_test import test_cli_commad
+
+
+if __name__ == "__main__":
+	
+	ser = serial.Serial('COM5', timeout=0.2)
+	print(ser.name)
+
+	openning_sequence = [1,1,0,8,8,1]
+	
+	segment = []
+
+	ser.write(openning_sequence)
+	time.sleep(0.1)
+
+	ser.write("GetPlcStatus\r\n")
+	time.sleep(0.1)
+	n_bytes = ser.in_waiting
+	response = ser.read(n_bytes)
+	
+	test_cli_commad(ser, "RegDbgVar 5\r\n{}{}{}{}{}".format(0), 1)
+
+	ser.close()
