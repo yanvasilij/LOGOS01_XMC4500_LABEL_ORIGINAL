495ebd1b26cac1bf5b104997b00469920fc82a5e
Vasilij Yanikeev <yanvasilij@gmail.com>
2018-09-29 23:52:22 +0500

    Added pointers init to cap build

--- Beremiz_RTE/src/user_app_process.c
+++ Beremiz_RTE/src/user_app_process.c
@@ -9,6 +9,9 @@
 #ifdef USE_USER_APP_CAP
 extern int startPLC(void);
 void runPLC(void);
+extern void on_board_io_init (plc_variables_t * variables, plc_configuration_t * configuration );
+static plc_variables_t vars;
+static plc_configuration_t conf;
 #endif
 
 
@@ -69,6 +72,7 @@ void poll_user_app (plc_variables_t * variables, plc_configuration_t * configura
 #ifdef USE_USER_APP_CAP
 	if (first_call)
 	{
+		on_board_io_init(&vars, &conf);
 		first_call = false;
 		startPLC();
 	}
